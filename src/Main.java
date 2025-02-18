30357fe3e7246f900d4ac6a7dcc85a07ea46212c
EduardoCamillo <eduardo.camillo15@gmail.com>
2025-02-18 15:31:13 -0300

    Implemented the registration function

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@ import java.io.*;
 import java.lang.annotation.Target;
 import java.util.Scanner;
 
-
 public class Main {
 
     public static void cadastraUser(String nome,String email,File myFile) throws IOException {
-            FileWriter fw = new FileWriter(myFile);
-            fw.write(nome + " " +  email);
+            FileWriter fw = new FileWriter(myFile, true);
+            fw.write("\n" + nome + " " +  email);
             fw.close();
             System.out.println("Usuário cadastrado com sucesso!");
     }
@@ -17,36 +16,49 @@ public class Main {
     }
     public static boolean tryLogin(String email, File myFile) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(myFile));
-        while(bufferedReader.readLine() != null) {
-            if (bufferedReader.readLine().contains(email)) {
-                System.out.println("Login realizado com sucesso!");
-                bufferedReader.close();
-                return false;
-            } else {
-                System.out.println("User não encontrado");
-                bufferedReader.close();
-                return true;
-            }
-        }
-        return false;
-    }
+        String line = bufferedReader.readLine();
+        if (line != null) {
+            while(line != null) {
+                if (line.contains(email)) {
+                    System.out.println("Login realizado com sucesso!");
+                    return true;
+                } else {
+                    System.out.println("User não encontrado");
 
+                    return false;
+                }
+            }
+        }else {
+            System.out.println("Está nulo!");
+            return false;
+        }
+        return true;
+    }
 
     public static void main(String[] args) throws IOException {
         File myFile = new File("./src/teste.txt");
-        FileWriter fileWriter = new FileWriter(myFile, true);
-        FileReader fileReader = new FileReader(myFile);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String name = "Camillao";
+        int selector;
+        String name = "eduardo";
+        Scanner scanner = new Scanner(System.in);
         Scanner sc = new Scanner(myFile);
         if(myFile.exists()){
             System.out.println("O arquivo existe mesmo: ");
-            tryLogin(name,myFile);
+            boolean bool = tryLogin(name,myFile);
+            if(!bool){
+                System.out.println("Usuário não encontrado, deseja efetuar o cadastro?\n" +
+                        "Digite 1 para sim ou qualquer tecla para não: ");
+                selector = scanner.nextInt();
+                if(selector == 1){
+                    System.out.println("Cadastrando usuário " + name + "...");
+                    cadastraUser(name, "opa@gmail", myFile);
+                }
+                else{
+                    System.out.println("Gracias!");
+                }
+            }
         }
         else{
             System.out.println("O arquivo não existe");
         }
-
    }
-
 }
