3e5b42a6be436a854839972218815e3da34ead2d
EduardoCamillo <eduardo.camillo15@gmail.com>
2025-02-28 14:48:15 -0300

    #2 - Implement UPDATE_USER function

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.lang.model.util.ElementScanner6;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -5,53 +6,6 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void cadastraUser(Users user,File myFile) throws IOException {
-           FileWriter fw = new FileWriter(myFile, true);
-
-           fw.write(user.getName() + " " +  user.getEmail() + "   ");
-           fw.close();
-           System.out.println("Usuário cadastrado com sucesso!");
-
-    }
-    //in progress:
-    public static void updateUsers(String name, File myFile, String newName) throws IOException {
-        FileWriter fw = new FileWriter(myFile, true);
-        String teste = new String(Files.readAllBytes(myFile.toPath()));
-        System.out.println("Alterando " + name + " para " + newName + "...");
-        String alterado = teste.replace(name, newName);
-        fw.write(alterado);
-        fw.close();
-
-
-        }
-
-    public static void deletaUsers(File myFile) throws IOException {
-            FileWriter fw = new FileWriter(myFile);
-            FileReader rd = new FileReader(myFile);
-            fw.flush();
-            fw.close();
-    }
-    public static boolean tryLogin(String email, File myFile) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(myFile));
-        String line = bufferedReader.readLine();
-        if (line != null) {
-            while(line != null) {
-                if (line.contains(email)) {
-                    System.out.println("Login realizado com sucesso!");
-                    return true;
-                } else {
-                    System.out.println("User não encontrado");
-                    return false;
-                }
-            }
-        }else {
-            System.out.println("Está nulo!");
-            return false;
-        }
-        return true;
-    }
-
     public static void main(String[] args) throws IOException {
         File myFile = new File("./src/teste.txt");
         System.out.println(myFile.toPath());
@@ -64,19 +18,19 @@ public class Main {
             System.out.println("Agora digite seu email: ");
             String email = scanner.nextLine();
             Users users = new Users(name,email);
-            boolean bool = tryLogin(users.getName(),myFile);
+            boolean bool = users.tryLogin(users.getName(),myFile);
             if(!bool){
                 System.out.println("Usuário não encontrado, deseja efetuar o cadastro?\n" +
                         "Digite 1 para sim ou 2 para apagar todos usuários ou 3 para editar um usuário: ");
                 selector = scanner.nextInt();
                 if(selector == 1){
-                    //System.out.println("Digite seu email: ");
-                    //email = scanner.next();
                     System.out.println("Cadastrando usuário " + users.getEmail() + "...");
-                    cadastraUser(users, myFile);
+                    users.cadastraUser(users, myFile);
+                    return;
                 }
-                if(selector == 2){
-                    deletaUsers(myFile);
+                else if(selector == 2){
+                    users.deletaUsers(myFile);
+                    return;
                 }
                 else{
                     System.out.println("Gracias!");
@@ -86,7 +40,15 @@ public class Main {
             System.out.println("---------------------------");
             System.out.println("Você está logado");
             System.out.println("---------------------------");
+            System.out.println("Digite 1 para atualizar seu usuário ou 2 para sair: ");
+            selector = scanner.nextInt();
+            if(selector == 1){
+                users.updateUsers(users.getName(), myFile, "NOVOnOME");
             }
+            else{
+                return;
+            }
+        }
         else{
             System.out.println("O arquivo não existe");
         }
--- src/Users.java
+++ src/Users.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.nio.file.Files;
+
 public class Users {
     private String name;
     private String email;
@@ -17,6 +20,49 @@ public class Users {
     }
     public String getEmail(){
         return this.email;
+    }
+    public void cadastraUser(Users user, File myFile) throws IOException {
+        FileWriter fw = new FileWriter(myFile, true);
+
+        fw.write(user.getName() + " " +  user.getEmail() + " ");
+        fw.close();
+        System.out.println("Usuário cadastrado com sucesso!");
+
+    }
+    //in progress:
+    public void updateUsers(String name, File myFile, String newName) throws IOException {
+        FileWriter fw = new FileWriter(myFile, true);
+        String teste = new String(Files.readAllBytes(myFile.toPath()));
+        System.out.println("Alterando " + name + " para " + newName + "...");
+        String alterado = teste.replaceAll(name, " d");
+        deletaUsers(myFile);
+        fw.write(alterado);
+        fw.close();
+    }
+    public void deletaUsers(File myFile) throws IOException {
+        FileWriter fw = new FileWriter(myFile);
+        System.out.println("Deletando usuário");
+        fw.flush();
+        fw.close();
+    }
+    public static boolean tryLogin(String email, File myFile) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(myFile));
+        String line = bufferedReader.readLine();
+        if (line != null) {
+            while(line != null) {
+                if (line.contains(email)) {
+                    System.out.println("Login realizado com sucesso!");
+                    return true;
+                } else {
+                    System.out.println("User não encontrado");
+                    return false;
+                }
+            }
+        }else {
+            System.out.println("Está nulo!");
+            return false;
+        }
+        return true;
     }
 
 }
